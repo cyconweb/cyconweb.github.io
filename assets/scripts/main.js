cd036ea49ef843f54382bd8af5dae7dd0159f858
cyconweb <cyconweb@yahoo.com>
2025-05-09 20:32:01 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,13 @@
+console.log("Hello world")
+
+let myVar = "Web Developer"
+const varDude = ["crap","megs","laugh"]
+
+console.log(varDude[varDude.length-1])
+
+
+function myFunc(x) {
+    return "Welcome "+x
+}
+
+console.log(myFunc("Hoopla"))
